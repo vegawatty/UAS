a79dd01849241f4a93717201ec1af102acfc1491
vegawatty <125202939+vegawatty@users.noreply.github.com>
2023-02-13 00:49:17 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Home</h2>
+	<p>Welcome to ITB STIKOM AMBON!</p>
+</div>
+
+<?=template_footer()?>
